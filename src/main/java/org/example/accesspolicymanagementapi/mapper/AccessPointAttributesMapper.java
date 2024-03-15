505783ce64e223d297394c5c4ac41ef1c14a3477
Jalal Assaly <jalal.assaly@gmail.com>
2024-03-15 15:55:43 +0200

    Repair Mapper for nested use

--- src/main/java/org/example/accesspolicymanagementapi/mapper/AccessPointAttributesMapper.java
+++ src/main/java/org/example/accesspolicymanagementapi/mapper/AccessPointAttributesMapper.java
@@ -7,6 +7,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface AccessPointAttributesMapper {
     AccessPointAttributesModel toModel(final AccessPointAttributes accessPointAttributes);
-
     AccessPointAttributes toDocument(final AccessPointAttributesModel accessPointAttributesModel);
 }
--- src/main/java/org/example/accesspolicymanagementapi/mapper/AccessPolicyMapper.java
+++ src/main/java/org/example/accesspolicymanagementapi/mapper/AccessPolicyMapper.java
@@ -3,13 +3,18 @@ package org.example.accesspolicymanagementapi.mapper;
 import org.example.accesspolicymanagementapi.documents.AccessPolicy;
 import org.example.accesspolicymanagementapi.models.AccessPolicyModel;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.ERROR ,
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR,
         uses = {UserAttributesMapper.class, AccessPointAttributesMapper.class})
 public interface AccessPolicyMapper {
 
+    @Mapping(source = "userAttributesSet", target="userAttributesModelSet")
+    @Mapping(source = "accessPointAttributes", target="accessPointAttributesModel")
     AccessPolicyModel toModel(final AccessPolicy accessPolicy);
 
+    @Mapping(source = "userAttributesModelSet", target="userAttributesSet")
+    @Mapping(source = "accessPointAttributesModel", target="accessPointAttributes")
     AccessPolicy toDocument(final AccessPolicyModel accessPolicyModel);
 }
