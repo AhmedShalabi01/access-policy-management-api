a373b0bca4c279c2e442fd720ce13257961abd48
Ahmed Shalabi <shtk3eb01@gmail.com>
2024-03-15 16:44:40 +0200

    Add the webclient exception handler and the validation exception handler

---
+++ src/main/java/org/example/accesspolicymanagementapi/exceptionhandler/responsebodies/ValidationExceptionBody.java
@@ -0,0 +1,31 @@
+package org.example.accesspolicymanagementapi.exceptionhandler.responsebodies;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.ValidationException;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatusCode;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ValidationExceptionBody {
+
+    @JsonProperty("timestamp")
+    private final LocalDateTime dateTime;
+    @JsonProperty("status")
+    private final Integer status;
+    @JsonProperty("message")
+    private String errorMessage;
+
+
+    public ValidationExceptionBody(HttpStatusCode status, ValidationException exception) {
+        this.dateTime = LocalDateTime.now();
+        this.status = status.value();
+        this.errorMessage = exception.getMessage();
+    }
+}
