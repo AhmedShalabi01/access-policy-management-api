9dba6923eb62b8552e1fb93f670bb6ba9013a737
Jalal Assaly <jalal.assaly@gmail.com>
2024-03-15 15:56:18 +0200

    Add @Data and @AllArgsConstructor

--- src/main/java/org/example/accesspolicymanagementapi/documents/AccessPointAttributes.java
+++ src/main/java/org/example/accesspolicymanagementapi/documents/AccessPointAttributes.java
@@ -1,7 +1,11 @@
 package org.example.accesspolicymanagementapi.documents;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.data.mongodb.core.index.Indexed;
 
+@Data
+@AllArgsConstructor
 public class AccessPointAttributes {
     @Indexed(unique = true)
     private String location;
