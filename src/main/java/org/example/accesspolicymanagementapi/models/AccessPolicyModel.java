aa9861aa30d3b3c77558426329ec6700db01f90c
Ahmed Shalabi <shtk3eb01@gmail.com>
2024-03-18 00:24:59 +0200

    Code refactor and enhance code logic

--- src/main/java/org/example/accesspolicymanagementapi/models/AccessPolicyModel.java
+++ src/main/java/org/example/accesspolicymanagementapi/models/AccessPolicyModel.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
