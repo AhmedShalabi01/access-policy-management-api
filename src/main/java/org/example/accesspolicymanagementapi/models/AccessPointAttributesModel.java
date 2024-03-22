c65a7855b0debe9accae5d9ae6b7d974eb9d23c8
Ahmed Shalabi <shtk3eb01@gmail.com>
2024-03-22 23:12:03 +0200

    Update the validation from @NotEmpty to @NotBlank

--- src/main/java/org/example/accesspolicymanagementapi/models/AccessPointAttributesModel.java
+++ src/main/java/org/example/accesspolicymanagementapi/models/AccessPointAttributesModel.java
@@ -1,17 +1,16 @@
 package org.example.accesspolicymanagementapi.models;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class AccessPointAttributesModel {
-    @NotEmpty(message = "The location field Can not be blank")
+    @NotBlank(message = "The location field Can not be blank")
     private String location;
-    @NotNull(message = "The occupancy level field Can not be blank")
+    @NotBlank(message = "The occupancy level field Can not be blank")
     @Min(value = 1,message = "The occupancy level can not be zero or less")
     private Integer occupancyLevel;
 }
