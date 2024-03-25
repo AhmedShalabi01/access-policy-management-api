3d249a7eaa6914f28c47190f55bfd0e52f014a8d
Ahmed Shalabi <shtk3eb01@gmail.com>
2024-03-25 15:15:35 +0200

    Update the validation for the sets from @NotBlank to @NotEmpty and for the minimumYearsOfExperience also to @NotNull

--- src/main/java/org/example/accesspolicymanagementapi/models/UserAttributesModel.java
+++ src/main/java/org/example/accesspolicymanagementapi/models/UserAttributesModel.java
@@ -2,6 +2,8 @@ package org.example.accesspolicymanagementapi.models;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -12,13 +14,13 @@ import java.util.Set;
 public class UserAttributesModel {
     @NotBlank(message = "The department field Can not be blank")
     private String department;
-    @NotBlank(message = "The allowed roles can not be empty")
+    @NotEmpty(message = "The allowed roles can not be empty")
     private Set<String> allowedRoles;
-    @NotBlank(message = "The minimum years of experience field Can not be blank")
+    @NotNull(message = "The minimum years of experience field Can not be blank")
     @Min(value = 1,message = "The minimum years of experience field Can not be blank")
     private Integer minimumYearsOfExperience;
-    @NotBlank(message = "The allowed clearance levels roles can not be empty")
+    @NotEmpty(message = "The allowed clearance levels roles can not be empty")
     private Set<String> allowedClearanceLevels;
-    @NotBlank(message = "The allowed employment status can not be empty")
+    @NotEmpty(message = "The allowed employment status can not be empty")
     private Set<String> allowedEmploymentStatus;
 }
