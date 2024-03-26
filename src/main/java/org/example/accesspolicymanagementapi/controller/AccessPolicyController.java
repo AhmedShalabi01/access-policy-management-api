faa2d0ffa271999cdc630e5cd7d7a38ffc111c9f
Jalal Assaly <jalal.assaly@gmail.com>
2024-03-26 20:37:46 +0200

    Correct typo in controller uri

--- src/main/java/org/example/accesspolicymanagementapi/controller/AccessPolicyController.java
+++ src/main/java/org/example/accesspolicymanagementapi/controller/AccessPolicyController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/access-polices")
+@RequestMapping("/access-policies")
 public class AccessPolicyController {
 
     private final AccessPolicyService accessPolicyService;
