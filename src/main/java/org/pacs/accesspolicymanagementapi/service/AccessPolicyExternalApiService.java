a2cb77810e1d3b4c922bab2d9e7df652d0a2d4f9
AhmedShalabi01 <shtk3eb01@gmail.com>
2024-05-20 17:33:27 +0300

    Add load balancer

--- src/main/java/org/pacs/accesspolicymanagementapi/service/AccessPolicyExternalApiService.java
+++ src/main/java/org/pacs/accesspolicymanagementapi/service/AccessPolicyExternalApiService.java
@@ -1,6 +1,7 @@
 package org.pacs.accesspolicymanagementapi.service;
 
 import org.pacs.accesspolicymanagementapi.models.LiveAccessPointAttributesModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -9,11 +10,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class AccessPolicyExternalApiService {
+
     private final WebClient webClient;
 
-    public AccessPolicyExternalApiService() {
-        this.webClient = WebClient.builder()
-                .baseUrl("http://localhost:8082/access-points-attributes")
+    @Autowired
+    public AccessPolicyExternalApiService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder
+                .baseUrl("http://ATTRIBUTES-MANAGEMENT-API/attributes-management/access-points-attributes")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
